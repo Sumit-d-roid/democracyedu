2d025b0600bbc4197a115a1038a5a725d485a2e7
Sumit-d-roid <sumitacharya9841@gmail.com>
2025-09-14 03:45:39 +0000

    improve the ci/cd

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/tests/',
+        '**/*.d.ts',
+        '**/*.test.{ts,tsx}',
+        '**/*.config.{ts,js}',
+      ],
+    },
+  },
+});
